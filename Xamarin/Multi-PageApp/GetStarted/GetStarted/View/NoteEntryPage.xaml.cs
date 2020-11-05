78be87df864be11eda14225a54877072ad957fca
RachidiL <lubaki-rachidi@outlook.com>
2020-11-05 15:13:49 +0100

    Xamarin Get-Started: Navigating between pages - Multi-Page

---
+++ Xamarin/Multi-PageApp/GetStarted/GetStarted/View/NoteEntryPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using GetStarted.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GetStarted.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void deleteButton_Clicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (File.Exists(note.FileName))
+                File.Delete(note.FileName);
+
+            await Navigation.PopAsync();
+        }
+
+        private async void saveButton_Clicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            if (string.IsNullOrWhiteSpace(note.FileName))
+            {
+                var fileName = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.text");
+                File.WriteAllText(fileName, note.Text);
+            }
+            else
+                File.WriteAllText(note.FileName, note.Text);
+
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ Xamarin/Multi-PageApp/GetStarted/GetStarted/View/NotesPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using GetStarted.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GetStarted.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var notes = new List<Note>();
+            var files = Directory.EnumerateFiles(App.FolderPath, "*.notes.text");
+            foreach (var fileName in files)
+            {
+                notes.Add(new Note
+                {
+                    FileName = fileName,
+                    Text = File.ReadAllText(fileName),
+                    Date = File.GetCreationTime(fileName)
+                });
+            }
+
+            notePageListView.ItemsSource = notes.OrderBy(d => d.Date).ToList();
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NoteEntryPage
+            {
+                BindingContext = new Note()
+            });
+        }
+
+        private async void notePageListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new NoteEntryPage
+                {
+                    BindingContext = e.SelectedItem as Note
+                });
+            }
+        }
+    }
+}
