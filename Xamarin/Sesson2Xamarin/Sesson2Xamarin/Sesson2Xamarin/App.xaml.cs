ac1da196d304b2f772a55d09bb9890c9324aa705
RachidiL <lubaki-rachidi@outlook.com>
2020-11-17 11:48:18 +0100

    DetailView - ListViewItemTapped

--- Xamarin/Sesson2Xamarin/Sesson2Xamarin/Sesson2Xamarin/App.xaml.cs
+++ Xamarin/Sesson2Xamarin/Sesson2Xamarin/Sesson2Xamarin/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Sesson2Xamarin
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- Xamarin/Sesson2Xamarin/Sesson2Xamarin/Sesson2Xamarin/Views/MainPage.xaml.cs
+++ Xamarin/Sesson2Xamarin/Sesson2Xamarin/Sesson2Xamarin/Views/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Sesson2Xamarin.Models;
+using Sesson2Xamarin.Views;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -24,20 +25,27 @@ namespace Sesson2Xamarin
             Items.Add(new ListViewItem { 
                 Name = "Test1",
                 Value = 10,
-                Text = "Blablabla"});
+                Text = "Blablabla This is View 1"});
             Items.Add(new ListViewItem
             {
                 Name = "Test2",
                 Value = 15,
-                Text = "Blablabla"
+                Text = "Blablabla this is View 2"
             });
             Items.Add(new ListViewItem
             {
                 Name = "Test3",
                 Value = 20,
-                Text = "Blablabla"
+                Text = "Blablabla this is View 3"
             });
             listViewExample.ItemsSource = Items;
+            listViewExample.ItemTapped += ListViewExample_ItemTapped;
+        }
+
+        private void ListViewExample_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            ListViewItem item = (ListViewItem)e.Item;
+            Navigation.PushAsync( new DetailView(item));
         }
     }
 }
