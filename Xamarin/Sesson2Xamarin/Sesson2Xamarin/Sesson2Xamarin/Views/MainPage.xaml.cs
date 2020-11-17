2c17188ef0b796bad8bdce23178340db828e23f5
RachidiL <lubaki-rachidi@outlook.com>
2020-11-17 11:28:44 +0100

    Added ListView

---
+++ Xamarin/Sesson2Xamarin/Sesson2Xamarin/Sesson2Xamarin/Views/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Sesson2Xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        List<string> Items;
+        public MainPage()
+        {
+            InitializeComponent();
+            InitList();
+        }
+
+        private void InitList()
+        {
+            Items = new List<string>();
+            Items.Add("Item1");
+            Items.Add("Item2");
+            Items.Add("Item3");
+            listViewExample.ItemsSource = Items;
+        }
+    }
+}
