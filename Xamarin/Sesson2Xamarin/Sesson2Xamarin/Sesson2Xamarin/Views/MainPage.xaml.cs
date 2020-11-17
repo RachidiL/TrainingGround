96e3081a75f51ad25f5e1499fd691738e4db9954
RachidiL <lubaki-rachidi@outlook.com>
2020-11-17 11:37:38 +0100

    ListView ItemTemplate

--- Xamarin/Sesson2Xamarin/Sesson2Xamarin/Sesson2Xamarin/Views/MainPage.xaml.cs
+++ Xamarin/Sesson2Xamarin/Sesson2Xamarin/Sesson2Xamarin/Views/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sesson2Xamarin.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -10,7 +11,7 @@ namespace Sesson2Xamarin
 {
     public partial class MainPage : ContentPage
     {
-        List<string> Items;
+        List<ListViewItem> Items;
         public MainPage()
         {
             InitializeComponent();
@@ -19,10 +20,23 @@ namespace Sesson2Xamarin
 
         private void InitList()
         {
-            Items = new List<string>();
-            Items.Add("Item1");
-            Items.Add("Item2");
-            Items.Add("Item3");
+            Items = new List<ListViewItem>();
+            Items.Add(new ListViewItem { 
+                Name = "Test1",
+                Value = 10,
+                Text = "Blablabla"});
+            Items.Add(new ListViewItem
+            {
+                Name = "Test2",
+                Value = 15,
+                Text = "Blablabla"
+            });
+            Items.Add(new ListViewItem
+            {
+                Name = "Test3",
+                Value = 20,
+                Text = "Blablabla"
+            });
             listViewExample.ItemsSource = Items;
         }
     }
