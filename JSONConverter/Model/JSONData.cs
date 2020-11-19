f95a17ae4f884f41ec5edc758db840436be6a719
Rachidi Lubaki <lubaki.r@eplan.de>
2020-11-19 09:25:09 +0100

    missing files

---
+++ JSONConverter/Model/JSONData.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace JSONConverter.Model
+{
+    class JSONData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string _jsonContent = "JSON File";
+        public string JSONContent 
+        {
+            get { return _jsonContent; }
+            set
+            {
+                _jsonContent = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ JSONConverter/ViewModel/JSONDataParser.cs
@@ -0,0 +1,19 @@
+﻿using JSONConverter.Model;
+using System.IO;
+
+namespace JSONConverter.ViewModel
+{
+    class JSONDataParser
+    {
+        JSONData _jsonData;
+        public JSONDataParser(JSONData jsonData)
+        {
+            _jsonData = jsonData;
+        }
+        public void ReadJSONFile(string fileName)
+        {
+            using var reader = new StreamReader(fileName);
+            _jsonData.JSONContent = reader.ReadToEnd();
+        }
+    }
+}
